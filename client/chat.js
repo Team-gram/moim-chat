6dab68565228459437a8a0d52a7729e7ba8f52dc
baegyu <baelanche@naver.com>
2022-11-22 10:46:57 +0900

    Feature: 채팅 서버 샘플

---
+++ client/chat.js
@@ -0,0 +1,29 @@
+const socket = io();
+
+const id = 1234567890; // cookie 활용
+const moimId = 1;
+const nickname = "minsu";
+const chatList = document.querySelector(".chat_list");
+const chat = document.querySelector(".chat_input");
+const sendButton = document.querySelector(".send_btn");
+const createdAt = "2000-01-01";
+
+$(".send_btn").click(() => {
+    const data = {
+        id: id,
+        moimId: moimId,
+        name: nickname,
+        message: chat.value,
+        createdAt: createdAt
+    }
+    socket.emit("chat", data);
+})
+
+socket.on("chat", (data) => {
+    const li = document.createElement("li");
+    li.innerText = `${data.name} : ${data.message}`;
+    console.log(data);
+    chatList.appendChild(li);
+})
+
+console.log(socket);
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const socketIO = require('socket.io');
+const app = express();
+
+const chatRouter = require('./routes/chat'); // client
+const path = require("path"); // client
+app.use(express.static(path.join(__dirname, "client"))); // client
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use("/", chatRouter); // client
+
+const server = app.listen(3000, function() {
+    console.log('Server is working');
+})
+
+const io = socketIO(server);
+
+io.on("connection", (socket) => {
+    socket.on("chat", (data) => {
+        io.emit("chat", data);
+        console.log(`server - char : ${data}`)
+    })
+})
