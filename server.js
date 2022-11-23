4a4ba8a3ac740b9df97030d6d52cb68d07fe6292
baegyu <baelanche@naver.com>
2022-11-23 19:01:09 +0900

    BugFix: CORS

--- server.js
+++ server.js
@@ -3,27 +3,21 @@ const socketIO = require('socket.io');
 const app = express();
 const cors = require('cors');
 
-const chatRouter = require('./routes/chat'); // client
 const path = require("path"); // client
 app.use(express.static(path.join(__dirname, "client"))); // client
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-const whitelist = ["http://localhost:8080", "http://localhost:8081", "http://localhost:3000"];
+
 const corsOptions = {
-  origin: function(origin, callback){
-    const isWhitelisted = whitelist.indexOf(origin) !== -1;
-    callback(null, isWhitelisted);
-  },
-  credentials:true
-}
-// const corsOptions = {
-//     origin: ["http://localhost:8080", "http://localhost:8081", "http://localhost:3000"],
-//     credentials: true
-// };
+    origin: ["http://localhost:8080", "http://localhost:8081", "http://localhost:3000"],
+    credentials: true
+};
 app.use(cors(corsOptions));
 
-app.use("/", chatRouter); // client
+app.get("/", cors(corsOptions), (req, res) => {
+    res.render("index");
+})
 
 const server = app.listen(3000, function() {
     console.log('Server is working');
